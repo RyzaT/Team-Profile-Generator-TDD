54d1223e76d6445030baa8d5fbb60588ecffb3ae
RyzaT <ryantaylor50@hotmail.com>
2023-02-26 17:07:10 +0000

    start of index .js

--- starter/index.js
+++ starter/index.js
@@ -12,4 +12,28 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const employees =[]; // puts employees as an empty array
 
+// function to initialise the app
+const init = async()=>{
+    await createManager();
+    // Employee Questions
+    const employeeQuestions = [
+        {
+            type: "list",
+            message: "Which type of employee do you wish to add?",
+            name: "employeeType",
+            choices: [
+                {name:"Engineer", value:"engineer"},
+                {name:"Intern", value:"intern"},
+                {name:"None", value:"none"},
+            ]
+        }
+    ]
+    // generate questions
+    const {employeeType} = await inquirer.createPromptModule(employeeQuestions);
+    // 'none' compleates team
+    if(employeeType === "none"){
+        
+    }
+}
