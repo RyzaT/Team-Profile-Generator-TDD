8c5b070e60832878d817a3af6052a218d820a29c
RyzaT <ryantaylor50@hotmail.com>
2023-02-26 16:44:12 +0000

    Adds manager js

--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// Import the employee parent class
+const Employee = require("./Employee")
+
+//Engineer is an extension of emlpoyee class
+class Manager extends Employee{
+    constructor({name, id, email, office}) {
+        super({name, email, id}); // gets info from parent
+        this.office = office; // adds office
+        this.role = "Team Manager"; // 'role' will define job role
+    }
+    // return github user profile
+    getOffice(){
+        return this.office;
+    }
+    // return role 
+    getRole(){
+        return this.role
+    }
+}
+// export engineer class
+module.exports = Manager;
