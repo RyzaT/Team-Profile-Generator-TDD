1933f454320600e6469cca806093e9358e3d075d
RyzaT <ryantaylor50@hotmail.com>
2023-02-26 16:25:13 +0000

    construct employee js

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor ({name, id, email}){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    // retrieve the name for employee input
+    getName(){
+        return this.name;
+    }
+    // retrieve the id for employee input
+    getId(){
+        return this.id;
+    }
+    // retrieve the email for employee input
+    getEmail(){
+        return this.email;
+    }
+}
+// Export employee data for generateing html
+module.exports = Employee;
